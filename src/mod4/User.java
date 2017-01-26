43e84cb94448cfb28cf4627376c44abcdbfacea1
filipenkosan9 <filipenkosan9@mail.ru>
2017-01-26 22:18:43 +0200

    homeWork4.4

--- src/mod4/User.java
+++ src/mod4/User.java
@@ -4,11 +4,24 @@ package mod4;
  * Created by Alex on 20.01.2017.
  */
 public class User {
-    long id;
-    String name;
-    double balance;
-    int mounceOfEmployment;
-    String companyName;
-    int salary;
-    Bank bank;
+     private long id;
+     private String name;
+     private double balance;
+     private int mounceOfEmployment;
+     private String companyName;
+     private int salary;
+     private Bank bank;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", balance=" + balance +
+                ", mounceOfEmployment=" + mounceOfEmployment +
+                ", companyName='" + companyName + '\'' +
+                ", salary=" + salary +
+                ", bank=" + bank +
+                '}';
+    }
 }
